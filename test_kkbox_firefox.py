f33217795bcc42502869328e21316d91acaf29f7
lenny_cheng <langrisser1981@gmail.com>
2018-03-15 15:54:05 +0800

    test kkbox in firefox

---
+++ test_kkbox_firefox.py
@@ -0,0 +1,8 @@
+import subprocess
+
+# result = subprocess.run(['ffplay', '-autoexit', 'http://ibobby.ai.hinet.net:8888/tts/ch/synthesisRaw?inputText=%E8%AB%8B%E5%95%8F%E6%82%A8%E8%A6%81%E5%95%8F%E7%9A%84%E4%BD%8D%E7%BD%AE%EF%BC%9F'], stdout=subprocess.PIPE)
+# print(result.stdout)
+
+url = 'https://widget.kkbox.com/v1/?id=4kxvr3wPWkaL9_y3o_&type=song&terr=TW&lang=TC&autoplay=true&loop=true'
+result = subprocess.Popen(['firefox', url], stdout=subprocess.PIPE)
+print(result.stdout)
