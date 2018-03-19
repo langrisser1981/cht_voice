d097cd40719317d4899e19b12552fe11686893d7
lenny_cheng <langrisser1981@gmail.com>
2018-03-19 15:45:49 +0800

    完成中華電信連續偵測，以及播放歌曲

--- mqtt.py
+++ mqtt.py
@@ -1,20 +1,37 @@
+# https://stackoverflow.com/questions/7088672/pyaudio-working-but-spits-out-error-messages-each-time
+from ctypes import *
+
+# From alsa-lib Git 3fd4ab9be0db7c7430ebd258f2717a976381715d
+# $ grep -rn snd_lib_error_handler_t
+# include/error.h:59:typedef void (*snd_lib_error_handler_t)(const char *file, int line, const char *function, int err, const char *fmt, ...) /* __attribute__ ((format (printf, 5, 6))) */;
+# Define our error handler type
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+
+
+def py_error_handler(filename, line, function, err, fmt):
+    pass
+
+
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+
+asound = cdll.LoadLibrary('libasound.so')
+# Set error handler
+asound.snd_lib_error_set_handler(c_error_handler)
+
 import subprocess
 import paho.mqtt.client as mqtt
 import json
 import datetime
 import calendar
 import time
-import vlc
 
 import pyaudio
 import math
 import struct
 import wave
+import sys
 
-# Instance = vlc.Instance()
-# player = Instance.media_player_new()
-state = 0
-
+# 遠端連線相關的定義
 hostname = 'ibobby.ai.hinet.net'
 port = 8883
 venderId = 'compal'
@@ -25,6 +42,21 @@ req = 'ai/speaker/{0}/{1}/req'.format(venderId, deviceId)
 rsp = 'ai/speaker/{0}/{1}/rsp'.format(venderId, deviceId)
 asr = 'ai/speaker/{0}/{1}/asr'.format(venderId, deviceId)
 asr_debug = 'ai/speaker/{0}/{1}/asr_debug'.format(venderId, deviceId)
+
+# 聲音相關的定義
+# Assuming Energy threshold upper than 30 dB
+Threshold = 80
+SHORT_NORMALIZE = (1.0 / 32768.0)
+chunk = 2400
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+swidth = 2
+Max_Seconds = 3
+
+TimeoutSignal = int((RATE / chunk * Max_Seconds) + 2)
+Time = 0
+silence = True
 
 
 def increase():
@@ -76,101 +108,97 @@ def register():
 
 
 def serviceInvoke(text):
-    action = 'InvokeReq'
-    time = getUTC()
-    requestID = getReqId()
     json_payload = {
-        'Action': action,
-        'Time': time,
-        'RequestID': requestID,
+        'Action': 'InvokeReq',
+        'Time': getUTC(),
+        'RequestID': getReqId(),
         'Text': text
     }
     payload = json.dumps(json_payload, sort_keys=True, ensure_ascii=False)
-    print('我的問題是:' + text + ', 時間:' + time + ', 請求:' + requestID)
+    print('我的問題是:' + json_payload.get('Text') + ', 時間:' + json_payload.get('Time') + ', 請求:' + json_payload.get(
+        'RequestID'))
     return payload
 
 
 def on_connect(client, userdata, flag, rc):
-    print("連線結果:" + str(rc))
+    print("連線結果:" + ('成功' if rc == 0 else '失敗'))
     client.subscribe(rsp)
     client.subscribe(asr_debug)
     # testServer()
 
 
 def testServer():
-    text = '今天有那些行程'
     text = '今天新北市天氣如何'
-    text = '請問張惠妹是誰'
     text = '我要聽張惠妹的姊妹'
+    text = '今天有那些行程'
+    text = '請問張惠妹是誰'
     text = '今天仁寶的股價是多少'
     client.publish(req, serviceInvoke(text))
 
 
 def on_disconnect(client, userdata, rc):
-    print("斷線:" + str(rc))
+    print("發生斷線")
 
 
+# 處理遠端回傳的訊息
 def on_message(client, userdata, msg):
-    # print(msg.topic+", "+str(msg.payload))
+    # print(msg.topic + ", " + str(msg.payload))
     payload = json.loads(msg.payload.decode('utf-8'))
-    Action = payload.get('Action', 0)
-    if Action == 'InvokeReq':
-        text = payload.get('Text', 0)
-        print("使用者說了:" + text)
 
-    Commands = payload.get('Commands', 0)
-    if Commands != 0:
-        flag = False
+    end = False
+    flag = False
 
-        for ele in payload['Commands']:
-            # print(ele)
-            myType = ele.get('Type', 0)
-            if myType == '01':
-                tUrl = ele.get('tUrl', 0)
-                if tUrl != 0:
-                    print("說話中...:" + tUrl)
-                    subprocess.run(['ffplay', '-autoexit', tUrl])
-                    # result = subprocess.run(['ffplay', '-autoexit', tUrl], stdout=subprocess.PIPE)
-                    # print(result.stdout)
-                    global silence
-                    silence = True
-                    stream.start_stream()
-                    print("waiting for Speech")
+    if msg.topic == asr_debug:
+        if 'Text' in payload:
+            print("遠端收到使用者說了:" + payload.get('Text'))
 
-                    # Media = Instance.media_new(tUrl)
-                    # player.set_media(Media)
-                    # player.play()
-
+    elif msg.topic == rsp:
+        end = True
+        if 'Commands' in payload:
+            for ele in payload['Commands']:
+                # print(ele)
+                myType = ele.get('Type', 0)
+                if myType == '01':
                     content = ele.get('Content', 0)
                     if content != 0 and not flag:
                         flag = True
                         print("回應的文字:" + content)
 
-            elif myType == '04':
-                content = ele.get('Content', 0)
-                if content != 0 and not flag:
-                    flag = True
-                    print("回應的文字:" + content)
+                    tUrl = ele.get('tUrl', 0)
+                    print("說話中:" + tUrl)
+                    subprocess.run(['ffplay', '-autoexit', tUrl])
+                    # result = subprocess.run(['ffplay', '-autoexit', tUrl], stdout=subprocess.PIPE)
+                    # print(result.stdout)
 
-            else:
-                print(payload)
+                elif myType == '02':
+                    content = ele.get('Content', 0)
+                    if content != 0:
+                        print("歌曲資訊:" + content)
+
+                    url = 'https://widget.kkbox.com/v1/?id=4kxvr3wPWkaL9_y3o_&type=song&terr=TW&lang=TC&autoplay=true&loop=true'
+                    result = subprocess.Popen(['firefox', url], stdout=subprocess.PIPE)
+                    # print(result.stdout)
+
+                elif myType == '04':
+                    content = ele.get('Content', 0)
+                    if content != 0 and not flag:
+                        flag = True
+                        print("回應的文字:" + content)
+
+                else:
+                    print('沒有登記過的類型')
+                    print(ele)
+
+    else:
+        print('無法處理的主題')
+        print(payload)
+
+    if end:
+        sys.exit()
 
 
-# Assuming Energy threshold upper than 30 dB
-Threshold = 80
-
-SHORT_NORMALIZE = (1.0 / 32768.0)
-chunk = 2400
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 16000
-swidth = 2
-Max_Seconds = 3
-
-TimeoutSignal = int((RATE / chunk * Max_Seconds) + 2)
-silence = True
-Time = 0
-samples = []
+# 以上是訊息處理的程式
+# 底下是錄音相關的程式
 
 
 def GetStream(chunk):
@@ -193,8 +221,8 @@ def rms(frame):
 
 def WriteSpeech(WriteData):
     stream.stop_stream()
-    # stream.close()
-    # p.terminate()
+    stream.close()
+    p.terminate()
 
     FileNameTmp = 'REC/rec{0}.wav'.format(getLocalTime())
     wf = wave.open(FileNameTmp, 'wb')
@@ -215,15 +243,15 @@ def KeepRecord(TimeoutSignal, LastBlock):
             continue
         samples.append(data)
 
-        message = bytearray(b'\x01')
-        message.extend(data)
-        client.publish(asr, bytes(message))
+        payload = bytearray(b'\x01')
+        payload.extend(data)
+        client.publish(asr, bytes(payload))
         time.sleep(0.05)
 
-    print("end record ")
     client.publish(asr, b'\x02')
+    print("錄音結束")
 
-    # print("write to File")
+    # print("寫入音檔完成")
     WriteSpeech(samples)
 
 
@@ -246,56 +274,30 @@ stream = p.open(format=FORMAT,
                 input=True,
                 output=True,
                 frames_per_buffer=chunk)
+
 try:
     while True:
         client.loop(.1)
+
         if silence:
             try:
                 input = GetStream(chunk)
-
             except:
                 continue
-
             rms_value = int(rms(input))
-            print(rms_value)
+            # print(rms_value)
             if (rms_value > Threshold):
-                print('---:{0}'.format(rms_value))
                 silence = False
+                print('啟動音量:{0}'.format(rms_value))
                 LastBlock = input
-                print("Recording....")
+                print("開始錄音")
                 client.publish(asr, b'\x00')
                 KeepRecord(TimeoutSignal, LastBlock)
 
             Time = Time + 1
             if (Time > TimeoutSignal):
                 Time = 0
-                # print("Time Out No Speech Detected")
-    '''
-    '''
-
-    '''
-    text = input('請輸入任務')
-    client.publish(req, serviceInvoke(text))
-    client.loop_start()
-    client.loop_forever(retry_first_connection=True)
-    publish.single(req, payload, hostname=hostname, port=port)
-    '''
-    '''
-    info = p.get_host_api_info_by_index(0)
-    numdevices = info.get('deviceCount')
-    for i in range(0, numdevices):
-            if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-                print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
-    '''
-
-    # result = subprocess.run(['ffplay', '-autoexit', 'http://ibobby.ai.hinet.net:8888/tts/ch/synthesisRaw?inputText=%E8%AB%8B%E5%95%8F%E6%82%A8%E8%A6%81%E5%95%8F%E7%9A%84%E4%BD%8D%E7%BD%AE%EF%BC%9F'], stdout=subprocess.PIPE)
-    # print(result.stdout)
-
-    # url = 'https://widget.kkbox.com/v1/?id=4kxvr3wPWkaL9_y3o_&type=song&terr=TW&lang=TC&autoplay=true&loop=true'
-    # result = subprocess.Popen(['firefox', url], stdout=subprocess.PIPE)
-    # print(result.stdout)
-
+                # print("時間範圍內沒有偵測到聲音")
 
 except KeyboardInterrupt:
-    print('bye~')
-
+    print('鍵盤中斷')
