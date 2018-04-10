b982c3d847dbf0716312d8e6a484d0e2e833b9d7
lenny_cheng <langrisser1981@gmail.com>
2018-04-10 15:27:32 +0800

    modify silence detection

--- mqtt.py
+++ mqtt.py
@@ -1,3 +1,5 @@
+LANG_CODE = 'en-US'  # Language to use
+
 # https://stackoverflow.com/questions/7088672/pyaudio-working-but-spits-out-error-messages-each-time
 from ctypes import *
 
@@ -26,9 +28,12 @@ import calendar
 import time
 
 import pyaudio
+import wave
+import audioop
+from collections import deque
+import os
 import math
 import struct
-import wave
 import sys
 
 from kkbox_partner_sdk.auth_flow import KKBOXOAuth
@@ -59,19 +64,143 @@ asr = 'ai/speaker/{0}/{1}/asr'.format(venderId, deviceId)
 asr_debug = 'ai/speaker/{0}/{1}/asr_debug'.format(venderId, deviceId)
 
 # 聲音相關的定義
-# Assuming Energy threshold upper than 30 dB
-Threshold = 80
-SHORT_NORMALIZE = (1.0 / 32768.0)
-chunk = 2400
+# Microphone stream config.
+CHUNK = 2400  # CHUNKS of bytes to read each time from mic
+# CHUNK = 1024  # CHUNKS of bytes to read each time from mic
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 16000
-swidth = 2
-Max_Seconds = 3
+THRESHOLD = 2200  # The threshold intensity that defines silence
+# and noise signal (an int. lower than THRESHOLD is silence).
 
-TimeoutSignal = int((RATE / chunk * Max_Seconds) + 2)
-Time = 0
-silence = True
+SILENCE_LIMIT = 2# Silence limit in seconds. The max ammount of seconds where
+# only silence is recorded. When this time passes the
+# recording finishes and the file is delivered.
+
+PREV_AUDIO = 0.5  # Previous audio (in seconds) to prepend. When noise
+
+
+# is detected, how much of previously recorded audio is
+# prepended. This helps to prevent chopping the beggining
+# of the phrase.
+
+
+def audio_int(num_samples=50):
+    """ Gets average audio intensity of your mic sound. You can use it to get
+        average intensities while you're talking and/or silent. The average
+        is the avg of the 20% largest intensities recorded.
+    """
+
+    print('Getting intensity values from mic.')
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    values = [math.sqrt(abs(audioop.avg(stream.read(CHUNK), 4)))
+              for x in range(num_samples)]
+    values = sorted(values, reverse=True)
+    r = sum(values[:int(num_samples * 0.2)]) / int(num_samples * 0.2)
+    print('Finished ')
+    print('Average audio intensity is {0}'.format(r))
+    stream.close()
+    p.terminate()
+    return r
+
+
+def listen_for_speech(threshold=THRESHOLD, num_phrases=-1):
+    """
+    Listens to Microphone, extracts phrases from it and sends it to
+    Google's TTS service and returns response. a "phrase" is sound
+    surrounded by silence (according to threshold). num_phrases controls
+    how many phrases to process before finishing the listening process
+    (-1 for infinite).
+    """
+
+    # Open stream
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print('Listening mic. ')
+    audio2send = []
+    cur_data = ''  # current chunk  of audio data
+    rel = RATE / CHUNK
+    slid_win = deque(maxlen=int(SILENCE_LIMIT * rel) + 1)
+    # Prepend audio from 0.5 seconds before noise was detected
+    prev_audio = deque(maxlen=int(PREV_AUDIO * rel) + 1)
+    started = False
+    n = num_phrases
+    response = []
+
+    try:
+        while (num_phrases == -1 or n > 0):
+            cur_data = stream.read(CHUNK)
+            slid_win.append(math.sqrt(abs(audioop.avg(cur_data, 4))))
+            # print slid_win[-1]
+            if (sum([x > THRESHOLD for x in slid_win]) > 0):
+                if (not started):
+                    print('Starting record of phrase')
+                    client.publish(asr, b'\x00')
+                    started = True
+
+                payload = bytearray(b'\x01')
+                payload.extend(cur_data)
+                client.publish(asr, bytes(payload))
+                # time.sleep(0.05)
+
+                audio2send.append(cur_data)
+            elif (started is True):
+                print('Finished')
+                client.publish(asr, b'\x02')
+
+                # The limit was reached, finish capture and deliver.
+                filename = save_speech(list(prev_audio) + audio2send, p)
+                # Send file to Google and get response
+                # Remove temp file. Comment line to review.
+                # os.remove(filename)
+                # Reset all
+                started = False
+                slid_win = deque(maxlen=int(SILENCE_LIMIT * rel) + 1)
+                prev_audio = deque(maxlen=int(PREV_AUDIO * rel) + 1)
+                audio2send = []
+                n -= 1
+                # print('Listening ...')
+            else:
+                prev_audio.append(cur_data)
+
+    except KeyboardInterrupt:
+        print('鍵盤中斷')
+
+    print('Done recording')
+    stream.close()
+    p.terminate()
+
+    return response
+
+
+def save_speech(data, p):
+    """ Saves mic data to temporary WAV file. Returns filename of saved
+        file """
+
+    filename = 'REC/output_' + str(int(time.time()))
+    # writes data to WAV file
+    # 這邊會出錯，前面要加b宣告位元
+    data = b''.join(data)
+    wf = wave.open(filename + '.wav', 'wb')
+    wf.setnchannels(1)
+    wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+    wf.setframerate(16000)  # TODO make this value a function parameter?
+    wf.writeframes(data)
+    wf.close()
+    return filename + '.wav'
 
 
 def increase():
@@ -139,7 +268,9 @@ def on_connect(client, userdata, flag, rc):
     print("連線結果:" + ('成功' if rc == 0 else '失敗'))
     client.subscribe(rsp)
     client.subscribe(asr_debug)
+    subprocess.run(['aplay', 'wellcome.wav'])
     # testServer()
+    listen_for_speech(1800, 1)  # listen to mic.
 
 
 def testServer():
@@ -216,61 +347,6 @@ def on_message(client, userdata, msg):
 
 
 # 以上是訊息處理的程式
-# 底下是錄音相關的程式
-
-
-def GetStream(chunk):
-    return stream.read(chunk)
-
-
-def rms(frame):
-    count = len(frame) / swidth
-    format = "%dh" % (count)
-    shorts = struct.unpack(format, frame)
-
-    sum_squares = 0.0
-    for sample in shorts:
-        n = sample * SHORT_NORMALIZE
-        sum_squares += n * n
-
-    rms = math.pow(sum_squares / count, 0.5);
-    return rms * 1000
-
-
-def WriteSpeech(WriteData):
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    FileNameTmp = 'REC/rec{0}.wav'.format(getLocalTime())
-    wf = wave.open(FileNameTmp, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(WriteData))
-    wf.close()
-
-
-def KeepRecord(TimeoutSignal, LastBlock):
-    samples = []
-    samples.append(LastBlock)
-    for i in range(0, TimeoutSignal):
-        try:
-            data = GetStream(chunk)
-        except:
-            continue
-        samples.append(data)
-
-        payload = bytearray(b'\x01')
-        payload.extend(data)
-        client.publish(asr, bytes(payload))
-        time.sleep(0.05)
-
-    client.publish(asr, b'\x02')
-    print("錄音結束")
-
-    # print("寫入音檔完成")
-    WriteSpeech(samples)
 
 
 client = mqtt.Client()
@@ -285,37 +361,9 @@ client.on_message = on_message
 client.connect("ibobby.ai.hinet.net", 8883)
 # client.publish(req, register())
 
-p = pyaudio.PyAudio()
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                output=True,
-                frames_per_buffer=chunk)
-
 try:
     while True:
         client.loop(.1)
-
-        if silence:
-            try:
-                input = GetStream(chunk)
-            except:
-                continue
-            rms_value = int(rms(input))
-            # print(rms_value)
-            if (rms_value > Threshold):
-                silence = False
-                print('啟動音量:{0}'.format(rms_value))
-                LastBlock = input
-                print("開始錄音")
-                client.publish(asr, b'\x00')
-                KeepRecord(TimeoutSignal, LastBlock)
-
-            Time = Time + 1
-            if (Time > TimeoutSignal):
-                Time = 0
-                # print("時間範圍內沒有偵測到聲音")
 
 except KeyboardInterrupt:
     print('鍵盤中斷')
